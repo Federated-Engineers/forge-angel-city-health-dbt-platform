23c60914df858f42d5ffdde4845db041c2a4995a
Temiloluwa Awoyele <awoyeletemiloluwa@gmail.com>
2026-09-23 00:59:35 +0100

    add: create lifelcycle policy for images in ECR

--- infrastructure/eu-central-1/production/ecr.tf
+++ infrastructure/eu-central-1/production/ecr.tf
@@ -16,3 +16,54 @@ resource "aws_ecr_repository" "achs_dbt_repo" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "achs_dbt_repo_lifecycle_policy" {
+  repository = aws_ecr_repository.achs_dbt_repo.name
+
+  policy = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Keep only 2 tagged images in standard storage class, transition all others to archive",
+      "selection": {
+        "tagStatus": "tagged",
+        "tagPatternList": ["*"],
+        "countType": "imageCountMoreThan",
+        "countNumber": 2
+      },
+      "action": {
+        "type": "transition",
+        "targetStorageClass": "archive"
+      }
+    },
+    {
+      "rulePriority": 2,
+      "description": "Keep only 1 untagged image, expire all others.",
+      "selection": {
+        "tagStatus": "untagged",
+        "countType": "imageCountMoreThan",
+        "countNumber": 1
+      },
+      "action": {
+        "type": "expire"
+      }
+    },
+    {
+      "rulePriority": 3,
+      "description": "Expire images archived for more than 90 days",
+      "selection": {
+        "tagStatus": "any",
+        "storageClass": "archive",
+        "countType": "sinceImageTransitioned",
+        "countUnit": "days",
+        "countNumber": 90
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
