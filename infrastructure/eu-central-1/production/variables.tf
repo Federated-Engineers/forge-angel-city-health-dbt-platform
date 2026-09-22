bd5fd2971126722ae4a1af6e85359932a0fb5d80
Temiloluwa Awoyele <awoyeletemiloluwa@gmail.com>
2026-09-23 00:36:05 +0100

    fix: sub claim for repos created before 15 July

--- infrastructure/eu-central-1/production/variables.tf
+++ infrastructure/eu-central-1/production/variables.tf
@@ -2,9 +2,10 @@ variable "github_repos" {
   type        = list(string)
   description = "List of GitHub repositories allowed for OIDC authentication & ECR Deployment"
   # repo:<github_org | owner>@<github_ord_id | owner_id>/<repo_name>@<repo_id>:ref:refs/heads/branch_name
+  # repo:<github_org | owner>/<repo_name>@:ref:refs/heads/branch_name # If repo was created before 15th July use this format
   default = [
-    "repo:Federated-Engineers@242844222/forge-angel-city-health-dbt-platform@1240885359:ref:refs/heads/main",
-    "repo:Federated-Engineers@242844222/forge-angel-city-health-dbt-platform@1240885359:*" # To Do: This line for testing only, remove it after testing is done
+    "repo:Federated-Engineers/forge-angel-city-health-dbt-platform:ref:refs/heads/main",
+    "repo:Federated-Engineers/forge-angel-city-health-dbt-platform:ref:refs/heads/*" # To Do: This line for testing only, remove it after testing is done
   ]
   sensitive = false
   nullable  = false
